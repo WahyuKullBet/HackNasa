1bddc900f37b074e1cb340a827e02d3e5ca9072b
WahyuXD <115902571+WahyuKullBet@users.noreply.github.com>
2022-10-28 03:32:39 +0700

    Add files via upload

---
+++ Hengker.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/sh
+clear
+echo "|==============================================|" | lolcat
+echo "|===========•Welcome To My Tools•==============|" | lolcat
+echo "|==========•Hengker Tzy No Counter•============|" | lolcat
+echo "|=============•By WaGyo Ganteng•===============|" | lolcat
+echo "|==============================================|" | lolcat
+echo 
+echo "-----------------------------------------------------" | lolcat -a -d
+echo
+echo "•=================================•" | lolcat -a -d
+echo "La la la la la la la la la... lala" | lolcat -a -d
+echo "Jnck Pristal" | lolcat -a -d
+echo "Pristal Ton Mayo" | lolcat -a -d
+echo "Pristal Pler Mayo" | lolcat -a -d
+echo "Pristal Eh" | lolcat -a -d
+echo "Pristal Beuh"| lolcat -a -d
+echo "Pristal Eh" | lolcat -a -d
+echo "Pristal Beuh" | lolcat -a -d
+echo "Jnck Pristal" | lolcat -a -d
+echo "Pristal Ton Mayo" | lolcat -a -d
+echo "Pristal Pler Mayo" | lolcat -a -d
+echo "•=================================•" | lolcat  -a -d
+clear
+figlet Lo Heker | lolcat -a
+figlet Anjay | lolcat -a
+cmatrix
+fi
